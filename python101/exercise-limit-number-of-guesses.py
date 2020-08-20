0d08aca9a715a119a5e64cac75ba82edda69fc41
Alessandra Burckhalter <ale.lptc@gmail.com>
2020-08-19 23:08:08 -0400

    updated from vs

--- python101/exercise-limit-number-of-guesses.py
+++ python101/exercise-limit-number-of-guesses.py
@@ -3,9 +3,13 @@ n = random.randint(1, 10)
 
 print("I am thinking of a number between 1 and 10.")
 
+attempts = 5
 
-for x in range(5, -1, -1):
+for x in range(attempts, -1, -1):
+    
     q = int(input("\nWhat's the number? "))
+    
+    
     if q == n:
         print("Yes! You win!")
         break
@@ -16,9 +20,9 @@ for x in range(5, -1, -1):
         print(f"{q} is too low.")
         
     elif q > n:
-        print(f"{q} is too high.")
-        
-    elif x == -1:
-        print("You ran out of guesses!")
+        print(f"{q} is too high.")           
     
     print(f"You have {x} guesses left")
+
+else:
+    print("You ran out of guesses!")
